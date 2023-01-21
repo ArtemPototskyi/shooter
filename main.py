2e993f51a04302f6a25c00f5b3aec4b286325764
Artem Pototskyi <ar.pototskyi@gmail.com>
2023-01-21 11:07:01 +0200

    add background image

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import pygame
+import os
 pygame.init()
+
+def file_path(file_name):
+    folder_path = os.path.abspath(__file__ + "/..")
+    path = os.path.join(folder_path, file_name)
+    return path
 
 FPS = 40
 WIN_WIDTH = 700
 WIN_HEIGH = 500
 
-window = pygame.display.set_mode((WIN_HEIGH, WIN_WIDTH))
+window = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGH))
 clock = pygame.time.Clock()
+
+background = pygame.image.load(file_path("city_background.jpg"))
+background = pygame.transform.scale(background, (WIN_WIDTH, WIN_HEIGH))
 
 play = True
 game = True
@@ -17,5 +26,8 @@ while game == True:
         if event.type == pygame.QUIT:
             game = False
         
+    if play == True:
+        window.blit(background, (0, 0))
+
     clock.tick(FPS)
     pygame.display.update()
