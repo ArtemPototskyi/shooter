6340d250d8624d498952e0cf833c8cc252630beb
Artem Pototskyi <ar.pototskyi@gmail.com>
2023-01-28 11:02:56 +0200

    added class player

--- main.py
+++ main.py
@@ -21,15 +21,29 @@ class GameSprite(pygame.sprite.Sprite):
     def __init__(self, image, x, y, width, heigh, speed):
         super().__init__()
         self.image = pygame.image.load(file_path(image))
-        self.image = pygame.transform.scale(self.image, (width, heigh))
+        self.image = pygame.transform.scale(self.image, (width, height))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
         self.speed = speed
-
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))    
-        
+
+class Player(GameSprite):
+    def __init__(self, x, y, width, height, speed):
+        super().__init__(image, x, y, width, height, speed)
+    def update(self):
+        keys = pygame.get_pressed()
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            self.rect.x -= self.speed
+        elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            self.rect.x += self.speed
+
+    def fire():
+        pass
+
+player = Player("bullet.png", 300, 400, 70, 70, 5)
+
 play = True
 game = True
 
@@ -41,6 +55,8 @@ while game == True:
         
     if play == True:
         window.blit(background, (0, 0))
+
+        player.reset
 
     clock.tick(FPS)
     pygame.display.update()
